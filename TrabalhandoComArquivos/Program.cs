15802b86ab5ac57761868ee31dabbf668de726bb
Luidrocha <luidrocha@yahoo.com.br>
2021-12-01 11:53:55 -0300

    Trabalhando com Arquivos FeleInfo e File

---
+++ TrabalhandoComArquivos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+
+namespace TrabalhandoComArquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"c:\temp\file1.txt"; // Sem o Arroba teria que colocar // no caminho
+            string targetPath = @"c:\temp\file2.txt";
+
+            try
+            {
+                FileInfo fileinfo = new FileInfo(sourcePath);
+                fileinfo.CopyTo(targetPath);
+                string[] linhas = File.ReadAllLines(sourcePath); // Le as linhas do arquivo
+
+                foreach (string linha in linhas ) // Le as linhas do Vetor
+                {
+
+                    Console.WriteLine(linha);
+                }
+
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Ocorreu um erro: ");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
